40dbc2616f456129153da74180d92354115943b8
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-07-31 15:44:14 +0100

    Created Model WeatherProviderResult

---
+++ src/WeatherTest.Services/Models/WeatherProviderResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherTest.Services.Models
+{
+    public class WeatherProviderResult
+    {
+         public string Location { get; set; }
+         public List<double> TemperatureCelsius { get; set; }
+         public List<double> TemperatureFahrenheit { get; set; }
+         public List<double> WindSpeedKph { get; set; }
+         public List<double> WindSpeedMph { get; set; }
+    }
+}
