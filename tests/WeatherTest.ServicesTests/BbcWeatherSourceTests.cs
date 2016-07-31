ab482958fe65bc7d5ff0dd60b1bf1fe7ad4248cd
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-07-31 13:02:50 +0100

    Added BbcWeatherSource

---
+++ tests/WeatherTest.ServicesTests/BbcWeatherSourceTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System;
+using WeatherTest.Services;
+using Xunit;
+
+namespace WeatherTest.ServicesTests
+{
+    public class BbcWeatherSourceTests
+    {
+        [Fact]
+        public void GivenNullLocationThenThrowArgumentNullException()
+        {
+            var source = new BbcWeatherSource();
+
+            Action act = () => source.Get(null).Wait();
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
