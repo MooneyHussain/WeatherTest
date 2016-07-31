3c8bfc1905ea551679627dd6bad32665bfdeca55
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-07-31 13:02:36 +0100

    Added AccuWeatherSource

---
+++ src/WeatherTest.Services/IWeatherSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using WeatherTest.Services.Models;
+
+namespace WeatherTest.Services
+{
+    interface IWeatherSource
+    {
+        Task<WeatherSourceResult> Get(string location);
+    }
+}
---
+++ tests/WeatherTest.ServicesTests/AccuWeatherSourceTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System;
+using WeatherTest.Services;
+using Xunit;
+
+namespace WeatherTest.ServicesTests
+{
+    public class AccuWeatherSourceTests
+    {
+        [Fact]
+        public void GivenNullLocationThenThrowArgumentNullException()
+        {
+            var source = new AccuWeatherSource();
+
+            Action act = () => source.Get(null).Wait();
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
