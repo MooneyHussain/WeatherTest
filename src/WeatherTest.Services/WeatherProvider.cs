6852df47ecf77783c2c886ad147961e90205eee3
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-07-31 13:33:34 +0100

    Created WeatherProvider

---
+++ src/WeatherTest.Services/WeatherProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherTest.Services
+{ 
+    public class WeatherProvider : IProvideWeather
+    {
+        private readonly IEnumerable<IWeatherSource> _weatherSources;
+
+        public WeatherProvider(IEnumerable<IWeatherSource> weatherSources)
+        {
+            if (weatherSources == null)
+                throw new ArgumentNullException(nameof(weatherSources));
+
+            _weatherSources = weatherSources;
+        }
+    }
+}
---
+++ tests/WeatherTest.ServicesTests/WeatherProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using System;
+using WeatherTest.Services;
+using Xunit;
+
+namespace WeatherTest.ServicesTests
+{
+    public class WeatherProviderTests
+    {
+        [Fact]
+        public void GivenNullWeatherSourcesThenThrowArgumentNullException()
+        {
+            Action act = () => new WeatherProvider(null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }      
+    }
+}
