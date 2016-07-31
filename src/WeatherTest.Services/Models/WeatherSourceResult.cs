478c7b97a666eafcdb42d289fd17eaf2a9351978
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-07-31 13:04:27 +0100

    Added Model

---
+++ src/WeatherTest.Services/Models/WeatherSourceResult.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherTest.Services.Models
+{
+    public class WeatherSourceResult
+    {
+        string Location { get; set; }
+        double TemperatureCelsius { get; set; }
+        double TemperatureFahrenheit { get; set; }
+        double WindSpeedKph { get; set; }
+        double WindSpeedMph { get; set; }
+    }
+}
