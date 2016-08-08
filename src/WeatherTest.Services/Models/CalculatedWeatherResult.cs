68bd676792b7aad36eee741d693bf54d782646b5
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-08-08 21:33:07 +0100

    Created CalculatedWeatherResult & WeatherRequest model

---
+++ src/WeatherTest.Services/Models/CalculatedWeatherResult.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherTest.Services.Models
+{
+    public class CalculatedWeatherResult
+    {
+        public string Location { get; set; }
+        public double AggregatedTemperature { get; set; }
+        public double AggregatedWindSpeed { get; set; }
+        public TemperatureUnit TemperatureUnit { get; set; }
+        public SpeedUnit SpeedUnit { get; set; }
+    }
+}
---
+++ src/WeatherTest.Services/Models/WeatherRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherTest.Services.Models
+{
+    public class WeatherRequest
+    {
+        public string Location { get; set; }
+        public TemperatureUnit TemperatureUnit { get; set; }
+        public SpeedUnit SpeedUnit { get; set; }
+    }
+}
