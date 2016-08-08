15ac99986fcccd9065c7bfad09737a905651a022
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-08-08 21:37:52 +0100

    Updated WeatherCalculator to calculate temperature (celsius)

--- src/WeatherTest.Services/WeatherCalculator.cs
+++ src/WeatherTest.Services/WeatherCalculator.cs
@@ -1,16 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using WeatherTest.Services.Models;
 
 namespace WeatherTest.Services
 {
     public class WeatherCalculator : ICalculateWeather
     {
-        public void Calculate(WeatherProviderResult model)
+        public CalculatedWeatherResult Calculate(WeatherProviderResult model, WeatherRequest criteria)
         {
             if (model == null)
                 throw new ArgumentNullException(nameof(model));
 
+            if (criteria == null)
+                throw new ArgumentNullException(nameof(criteria));
+
+            var aggregatedTemp = CalculateTemperature(criteria.TemperatureUnit, model);
+
+            return new CalculatedWeatherResult
+            {
+                Location = criteria.Location,
+                AggregatedTemperature = aggregatedTemp,
+                TemperatureUnit = criteria.TemperatureUnit
+            };
+        }
+
+        private double CalculateTemperature(TemperatureUnit unit, WeatherProviderResult model)
+        {
+            double result;
+            var averagedCelsius = GetAverageResult(model.TemperatureCelsius);
+            var averagedFahrenheit = GetAverageResult(model.TemperatureFahrenheit);
+
+            if (unit == TemperatureUnit.Celsius)
+            {
+                var celsius = (averagedFahrenheit - 32) * 5 / 9;
+                result = (averagedCelsius + celsius) / 2;
+                return result;
+            }
+
             throw new NotImplementedException();
+        }
+               
+        private double GetAverageResult(List<double> numbers)
+        {
+            return numbers.Average();
         }
     }
 }
--- tests/WeatherTest.ServicesTests/WeatherCalculatorTests.cs
+++ tests/WeatherTest.ServicesTests/WeatherCalculatorTests.cs
@@ -1,21 +1,57 @@
 ﻿using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using WeatherTest.Services;
+using WeatherTest.Services.Models;
 using Xunit;
 
 namespace WeatherTest.ServicesTests
 {
     public class WeatherCalculatorTests
     {
+        private readonly string _location = "dorset";
+
         [Fact]
         public void GivenNullModelThenThrowArgumentNullException()
         {
             var weatherCalculator = new WeatherCalculator();
 
-            Action act = () => weatherCalculator.Calculate(null);
+            Action act = () => weatherCalculator.Calculate(null, new WeatherRequest());
 
             act.ShouldThrow<ArgumentNullException>();
         }
 
+        [Fact]
+        public void GivenNullWeatherRequestCriteriaThenThrowArgumentNullException()
+        {
+            var weatherCalculator = new WeatherCalculator();
+
+            Action act = () => weatherCalculator.Calculate(new WeatherProviderResult(), null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GivenCalculateIsInvokedWhenTemperatureUnitIsCelsiusThenReturnCorrectAggregatedResult()
+        {
+            var model = new WeatherProviderResult
+            {
+                Location = _location,
+                TemperatureCelsius = new List<double> { 10 },
+                TemperatureFahrenheit = new List<double> { 68 }
+            };
+
+            var criteria = new WeatherRequest
+            {
+                Location = _location,
+                TemperatureUnit = TemperatureUnit.Celsius
+            };
+
+            var weatherCalculator = new WeatherCalculator();
+
+            var result = weatherCalculator.Calculate(model, criteria);
+
+            result.AggregatedTemperature.Should().Be(15);
+        }
     }
 }
