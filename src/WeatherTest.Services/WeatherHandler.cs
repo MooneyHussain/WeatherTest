af33e140e1091649bb444bc0e99a5ef2b7dc06eb
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-08-09 21:37:44 +0100

    Created WeatherHandler + Tests (Constructor)

---
+++ src/WeatherTest.Services/WeatherHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WeatherTest.Services
+{
+    public class WeatherHandler
+    {
+        private readonly IProvideWeather _weatherProvider;
+        private readonly ICalculateWeather _weatherCalculator;
+
+        public WeatherHandler(IProvideWeather weatherProvider, ICalculateWeather weatherCalculator)
+        {
+            if (weatherProvider == null)
+                throw new ArgumentNullException(nameof(weatherProvider));
+
+            if (weatherCalculator == null)
+                throw new ArgumentNullException(nameof(weatherCalculator));
+
+            _weatherProvider = weatherProvider;
+            _weatherCalculator = weatherCalculator;
+        }
+    }
+}
---
+++ tests/WeatherTest.ServicesTests/WeatherHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using WeatherTest.Services;
+using Xunit;
+
+namespace WeatherTest.ServicesTests
+{
+    public class WeatherHandlerTests
+    {        
+        public class Constructor
+        {
+            private readonly Mock<IProvideWeather> _weatherProvider;
+            private readonly Mock<ICalculateWeather> _weatherCalculator;
+
+            public Constructor()
+            {
+                _weatherProvider = new Mock<IProvideWeather>();
+                _weatherCalculator = new Mock<ICalculateWeather>();
+            }
+
+            [Fact]
+            public void GivenNullWeatherProviderThenThrowArgumentNullException()
+            {
+                Action act = () => new WeatherHandler(null, _weatherCalculator.Object);
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+
+            [Fact]
+            public void GivenNullWeatherCalculatorThenThrowArgumentNullException()
+            {
+                Action act = () => new WeatherHandler(_weatherProvider.Object, null);
+
+                act.ShouldThrow<ArgumentNullException>();
+            }
+        }
+    }
+}
