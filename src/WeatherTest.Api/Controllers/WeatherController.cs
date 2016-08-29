ed7bf91f4b36e45e69e9705e856269ad45c22aa2
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-08-29 22:35:16 +0100

    Added IHandleWeather and null checks to WeatherController

--- src/WeatherTest.Api/Controllers/WeatherController.cs
+++ src/WeatherTest.Api/Controllers/WeatherController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using WeatherTest.Services;
 
 namespace WeatherTest.Api.Controllers
 {
-    [Route("api/[controller]")]
     public class WeatherController : Controller
     {
+        private readonly IHandleWeather _weatherHandler;
 
+        public WeatherController(IHandleWeather weatherHandler)
+        {
+            if (weatherHandler == null)
+                throw new ArgumentNullException(nameof(weatherHandler));
+
+            _weatherHandler = weatherHandler;
+        }
+
+        [HttpGet, Route("[controller]/{location}/{temperatureUnit}/{speedUnit}")]
+        public IActionResult Get(string location, string temperatureUnit, string speedUnit)
+        {
+            if (location == null) throw new ArgumentNullException(nameof(location));
+            if (temperatureUnit == null) throw new ArgumentNullException(nameof(temperatureUnit));
+            if (speedUnit == null) throw new ArgumentNullException(nameof(speedUnit));
+
+            throw new NotImplementedException();             
+        }
     }
 }
---
+++ tests/WeatherTest.ApiTests/WeatherControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using WeatherTest.Api.Controllers;
+using WeatherTest.Services;
+using Xunit;
+
+namespace WeatherTest.ApiTests
+{
+    public class WeatherControllerTests
+    {
+        private Mock<IHandleWeather> _weatherHandler;
+        private string _location = "bournemouth";
+        private string _tempUnit = "celsius";
+        private string _speedUnit = "mph";
+
+        public WeatherControllerTests()
+        {
+            _weatherHandler = new Mock<IHandleWeather>();
+        }
+
+        [Fact]
+        public void GivenNullWeatherHandleThenThrowArgumentNullException()
+        {
+            Action act = ()  => new WeatherController(null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+
+        [Fact]
+        public void GivenNullLocationThenThrowArgumentNullException()
+        {
+            var controller = new WeatherController(_weatherHandler.Object);
+
+            Action act = () => controller.Get(null, _tempUnit, _speedUnit);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GivenNullTemperatureUnitThenThrowArgumentNullException()
+        {
+            var controller = new WeatherController(_weatherHandler.Object);
+
+            Action act = () => controller.Get(_location, null, _speedUnit);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GivenNullSpeedUnitThenThrowArgumentNullException()
+        {
+            var controller = new WeatherController(_weatherHandler.Object);
+
+            Action act = () => controller.Get(_location, _tempUnit, null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
