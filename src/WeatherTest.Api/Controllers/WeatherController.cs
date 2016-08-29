490c0bfae2080d204e5390d408ccbbc72ef6e8da
Mooney Hussain <mhamunhussain1989@gmail.com>
2016-08-29 19:52:24 +0100

    Created WeatherController

---
+++ src/WeatherTest.Api/Controllers/WeatherController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherTest.Services;
+
+namespace WeatherTest.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+
+    }
+}
